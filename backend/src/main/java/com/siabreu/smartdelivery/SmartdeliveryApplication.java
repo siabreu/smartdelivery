381a293c840c4a2f6efae099929fd09ee82a1118
siabreu <siabreu@gmail.com>
2021-01-06 11:19:40 -0300

    Project created

---
+++ backend/src/main/java/com/siabreu/smartdelivery/SmartdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.siabreu.smartdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartdeliveryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/siabreu/smartdelivery/SmartdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siabreu.smartdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
